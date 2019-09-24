341636b4ddb4be9af1494ee7844f3937b4cc19de
Daniel Schmidt <dschmidt@mesosphere.com>
2019-09-24 22:27:01 +0200

    chore: remove dist

--- packages/plugins/typeguards/src/index.ts
+++ packages/plugins/typeguards/src/index.ts
@@ -1,3 +1,14 @@
-module.exports = function() {
+import { NodePath } from "@babel/traverse";
+import * as t from "@babel/types";
+
+type TypeDefinition = t.TSInterfaceDeclaration | t.TSTypeAliasDeclaration;
+type TypeDefinitionPath = NodePath<TypeDefinition>;
+
+type Config = {};
+
+module.exports = function(
+  _config: Config,
+  tsDefinitions: TypeDefinitionPath[]
+) {
   return [];
 };
